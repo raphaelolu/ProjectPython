9aeca0c7fb56580af8041118471d1dd2bd87e5b4
raphael olu <raphaelo@hotmail.co.uk>
2020-10-07 16:53:42 +0100

    working progress

--- words.py
+++ words.py
@@ -13,7 +13,35 @@ class Person:
     def name(self):
         return self._name
 
-    Score = 0
+    totalScore = 0
+
+    SpareValue = 0
+
+    WasASpare = 0
+
+    rounds = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
+
+    WasAStrike = 0
+
+    StrikeScore = 0
+
+    noSecondThrowBecauseStrike = 0
+
+    gameStats =   {1: [1, 2,   "x",4],
+                   2: [{0: 0}, "X",4],
+                   3: [{0: 0}, "X",4],
+                   4: [{0: 0}, "X",4],
+                   5: [{0: 0}, "X",4],
+                   6: [{0: 0}, "X",4],
+                   7: [{0: 0}, "X",4],
+                   8: [{0: 0}, "X",4],
+                   9: [{0: 0}, "X",4],
+                   10:[{0: 0}, "X",4]
+                   }
+
+
+raph = Person("raph")
+print(f" {raph.gameStats[1][0].values()} and {raph.gameStats[1][1]}")
 
 
 class Game:
@@ -28,59 +56,6 @@ class Game:
         intTheLead = ""
 
 
-        #
-        # counter = 0
-        # totalScore = 0
-        # getName = input("Hello please type your name to begin \n")
-        # player1 = Person(getName)
-        # while counter != 10:
-        #     counter += 1
-        #     if counter == 1:
-        #         print("Hello " + player1.name() + " Welcome to the python bowling game")
-        #         bowl = input("Please roll the ball by pressing and any key on the keypad\n")
-        #         firstThrow = random.randint(1, 10)
-        #         totalScore += firstThrow
-        #         if firstThrow > 5:
-        #             print(f"well done you scored a {firstThrow}")
-        #         else:
-        #             print(f"Unlucky scored a {firstThrow}")
-        #         bowl2 = input("Please roll second ball by any key on the keypad\n")
-        #         secondThrow = random.randint(1, 10)
-        #         totalScore += secondThrow
-        #         print(f"well done {player1.name()} you scored a {secondThrow} ")
-        #         print(f"your total score is  {totalScore}")
-        #         # TemporaryDictionary = [(f"round {counter}", totalScore)]
-        #         # player1.Rounds.update(TemporaryDictionary)
-        #         roundValue = {'firstThrow': firstThrow, 'secondThrow': secondThrow, 'TotalScore': totalScore}
-        #         player1.Rounds[f'round {counter}'] = roundValue
-        #     elif counter > 1:
-        #         print(f" Welcome to round {counter}")
-        #         bowl = input("Please roll ball by typing 1 on your keypad\n")
-        #         firstThrow = random.randint(1, 10)
-        #         totalScore += firstThrow
-        #         if firstThrow > 5:
-        #             print(f"well done you scored a {firstThrow}")
-        #         else:
-        #             print(f"Unlucky scored a {firstThrow}")
-        #         bowl2 = input("Please roll second ball by pressing any key on the  keypad\n")
-        #         secondThrow = random.randint(1, 10)
-        #         print(f"well done  you  scored a {secondThrow}")
-        #         totalScore += secondThrow
-        #         print(f"your total score so far is {totalScore}")
-        #         # TemporaryDictionary = [(f"round {counter}", totalScore)]
-        #         # player1.Rounds.update(TemporaryDictionary)
-        #         roundValue = {'firstThrow': firstThrow, 'secondThrow': secondThrow, 'TotalScore': totalScore}
-        #         player1.Rounds[f'round {counter}'] = roundValue
-        # print(f" Congrats {player1.name()} The game is now over, the results from the games were ...")
-        # for key, value in player1.Rounds.items():
-        #     print(f'{key} => {value}')
-        # print(f" The total score for the game is {totalScore}")
-        # player1.Score = totalScore
-        # jsonObject = json.dumps(player1.Rounds)
-        # print(jsonObject)
-        # return jsonObject
-
-
 def playGame():
     print("Hello welcome to the  boring python game ")
     getPlayerOne = input("Player one please type in your name")
@@ -93,56 +68,80 @@ def playGame():
     players.append(playerOne)
     players.append(playerTwo)
     game = Game(players)
+    temporaryValueHolder = 0
     counter = 0
     totalScore = 0
-    getName = input("Hello please type your name to begin \n")
-    player1 = Person(getName)
+
     while counter != 10:
         counter += 1
-        if counter == 1:
-            print("Hello " + player1.name() + " Welcome to the python bowling game")
-            bowl = input("Please roll the ball by pressing and any key on the keypad\n")
+        bowl = input(f" {playerOne.name()} Please roll the ball by pressing any key on the keypad\n")
+        firstThrow = random.randint(1, 10)
+        if firstThrow == 10:
+            print("that is a strike")
+            playerOne.gameStats[counter][0][1] = 1
+        # if playerOne.wasASpare == 1:
+        if  counter != 1 and playerOne.gameStats[counter -1][2] == 1:
+        # which would mean theres a spare
+            playerOne.gameStats[counter -1][0]
+            for key, value in playerOne.rounds.items():
+                if key == counter - 1
+                    value += firstThrow
+                    playerOne.wasASpare = 0
+        if firstThrow > 5:
+            print(f"well done you scored a {firstThrow}")
+        else:
+            print(f"Unlucky scored a {firstThrow}")
+        for key, value in playerOne.rounds.items():
+            if key == counter:
+                value += firstThrow
+        playerOne.totalScore += firstThrow
+        if playerOne.noSecondThrowBecauseStrike != 1:
+            if player on is has not got a x
+            bowl2 = input(f" {playerOne.name()} Please roll second ball by any key on the keypad\n")
+            secondThrow = random.randint(1, 10)
+            if secondThrow + firstThrow == 10:
+                print(f" well done {playerOne.name()} you have a spare ")
+                playerOne.wasASpare = 1
+                playerOne.totalScore += secondThrow
+            else:
+                for key, value in playerOne.rounds.items():
+                    if key == counter:
+                        value += secondThrow
+                        playerOne.totalScore += secondThrow
+            if playerOne
+            bowl3 = input(f" {playerTwo.name()} Please roll the ball by pressing  any key on the keypad\n")
             firstThrow = random.randint(1, 10)
             totalScore += firstThrow
             if firstThrow > 5:
-                print(f"well done you scored a {firstThrow}")
+                print(f"well done {playerTwo.name()} you scored a {firstThrow}")
             else:
-                print(f"Unlucky scored a {firstThrow}")
-            bowl2 = input("Please roll second ball by any key on the keypad\n")
+                print(f"Unlucky  {playerTwo.name()} you scored a {firstThrow}")
+            bowl4 = input(f"Please {playerTwo.name()} roll second ball by pressing any key on the keypad\n")
             secondThrow = random.randint(1, 10)
+            if player one that round - 1 has x then add both rounds to round -1
             totalScore += secondThrow
-            print(f"well done {player1.name()} you scored a {secondThrow} ")
-            print(f"your total score is  {totalScore}")
-            # TemporaryDictionary = [(f"round {counter}", totalScore)]
-            # player1.Rounds.update(TemporaryDictionary)
-            roundValue = {'firstThrow': firstThrow, 'secondThrow': secondThrow, 'TotalScore': totalScore}
-            player1.Rounds[f'round {counter}'] = roundValue
-        elif counter > 1:
-            print(f" Welcome to round {counter}")
-            bowl = input("Please roll ball by typing 1 on your keypad\n")
-            firstThrow = random.randint(1, 10)
-            totalScore += firstThrow
-            if firstThrow > 5:
-                print(f"well done you scored a {firstThrow}")
+            if secondThrow > 5:
+                print(f"well done {playerTwo.name()} you  scored a {secondThrow}")
             else:
-                print(f"Unlucky scored a {firstThrow}")
-            bowl2 = input("Please roll second ball by pressing any key on the  keypad\n")
-            secondThrow = random.randint(1, 10)
-            print(f"well done  you  scored a {secondThrow}")
-            totalScore += secondThrow
-            print(f"your total score so far is {totalScore}")
-            # TemporaryDictionary = [(f"round {counter}", totalScore)]
-            # player1.Rounds.update(TemporaryDictionary)
-            roundValue = {'firstThrow': firstThrow, 'secondThrow': secondThrow, 'TotalScore': totalScore}
-            player1.Rounds[f'round {counter}'] = roundValue
-    print(f" Congrats {player1.name()} The game is now over, the results from the games were ...")
-    for key, value in player1.Rounds.items():
-        print(f'{key} => {value}')
-    print(f" The total score for the game is {totalScore}")
-    player1.Score = totalScore
-    jsonObject = json.dumps(player1.Rounds)
-    print(jsonObject)
-    return jsonObject
+                print(f"unlucky {playerTwo.name()} you scored a {secondThrow}")
+                print(f"your total score is {totalScore}")
+                # TemporaryDictionary = [(f"round {counter}", totalScore)]
+                # player1.Rounds.update(TemporaryDictionary)
+                # roundValue = {'firstThrow': firstThrow, 'secondThrow': secondThrow, 'TotalScore': totalScore}
+                # player1.Rounds[f'round {counter}'] = roundValue
+        print(f"Good game {playerOne} and {playerTwo} the game is now over")
+        if playerOne.totalScore > playerTwo.totalScore:
+            print(f" Congrats {playerOne.name()} The game is now over, you are the winner")
+        elif playerTwo.totalScore > playerOne.totalScore:
+            print(f"well done {playerTwo} you have won the game")
+
+        for key, value in playerOne.Rounds.items():
+            print(f'{key} => {value}')
+        print(f" The total score for the game is {totalScore}")
+        player1.Score = totalScore
+        jsonObject = json.dumps(player1.Rounds)
+        print(jsonObject)
+        return jsonObject
 
 
 playGame()
